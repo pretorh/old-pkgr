f4057883dd70b70b2f3ebfe45a7aafaa6a4edcf5
Hendri Pretorius <pretorh@gmail.com>
2016-03-13 16:19:26 +0200

    query installed lists the directory contents of the library dir

--- src/commands/query.c
+++ src/commands/query.c
@@ -1,10 +1,12 @@
 #include "../commands.h"
+#include "../utils.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void query(struct Settings *settings) {
     if (strcmp(settings->argument, "installed") == 0) {
+        execute("ls %s/", settings->dir_library);
     } else {
         fprintf(stderr, "Unknown query option: %s\n", settings->argument);
         exit(EXIT_FAILURE);
