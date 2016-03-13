eaa210181933220d8b25139ecd2d0d615887eda2
Hendri Pretorius <pretorh@gmail.com>
2016-03-13 18:40:20 +0200

    extract function to validate files in package

--- src/pkgr/install.c
+++ src/pkgr/install.c
@@ -10,6 +10,8 @@
 #define MODE_755 S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH
 #define MKDIR_MODE MODE_755
 
+void validate_files_in_package(const char *library, const char *archive, const char *owned);
+
 void install(struct Settings *settings) {
     char name[50], package_dir[PATH_MAX + 1], owned[PATH_MAX + 1];
     package_get_name(settings->argument, name);
@@ -18,14 +20,20 @@ void install(struct Settings *settings) {
 
     mkdir(package_dir, MKDIR_MODE);
 
+    validate_files_in_package(settings->dir_library, settings->argument, owned);
+
+    package_extract_files(settings->argument, settings->dir_root);
+}
+
+void validate_files_in_package(const char *library, const char *archive, const char *owned) {
     FILE *fowned = fopen(owned, "w");
 
-    FILE *flist = package_read_file_list(settings->argument);
+    FILE *flist = package_read_file_list(archive);
     char file[LINE_MAX + 1];
     char current_owner[1000];
     int already_owned = 0;
     while (read_trimmed_line(flist, file)) {
-        if (library_get_owner(settings->dir_library, file, current_owner)) {
+        if (library_get_owner(library, file, current_owner)) {
             fprintf(stderr, "%s is currently owned by %s\n", file, current_owner);
             already_owned++;
         }
@@ -40,6 +48,4 @@ void install(struct Settings *settings) {
         fprintf(stderr, "%d files are already owned by other packages\n", already_owned);
         exit(EXIT_FAILURE);
     }
-
-    package_extract_files(settings->argument, settings->dir_root);
 }
