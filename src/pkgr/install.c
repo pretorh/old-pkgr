3b989ce7bafca0f626a788a316ae7e8d27e4db80
Hendri Pretorius <pretorh@gmail.com>
2016-03-13 17:38:40 +0200

    check if the files exists before extracting the package files
    print the files that exists and fail

--- src/pkgr/install.c
+++ src/pkgr/install.c
@@ -1,8 +1,10 @@
 #include "install.h"
+#include "library.h"
 #include "package.h"
 #include "../utils.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 
 #define MODE_755 S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH
@@ -28,12 +30,24 @@ void install(struct Settings *settings) {
 
     FILE *flist = package_read_file_list(settings->argument);
     char file[LINE_MAX + 1];
+    char current_owner[1000];
+    int already_owned = 0;
     while (read_trimmed_line(flist, file)) {
+        if (library_get_owner(settings->dir_library, file, current_owner)) {
+            fprintf(stderr, "%s is currently owned by %s\n", file, current_owner);
+            already_owned++;
+        }
+
         fprintf(fowned, "%s\n", file);
     }
     fclose(flist);
 
     fclose(fowned);
+
+    if (already_owned) {
+        fprintf(stderr, "%d files are already owned by other packages\n", already_owned);
+        exit(EXIT_FAILURE);
+    }
 
     package_extract_files(settings->argument, settings->dir_root);
 }
