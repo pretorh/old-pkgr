3deda7c7f516d2594ca1f6643c2cd1b5dc3c6c9b
Hendri Pretorius <pretorh@gmail.com>
2016-03-14 18:24:09 +0200

    check full path in test
    test using the full path of expected file

--- tests/install/files-owned.sh
+++ tests/install/files-owned.sh
@@ -8,5 +8,5 @@ package_files usr/bin/hello
 ./pkgr --install $PACKAGE_DIR/test-1.tar.xz --root-dir $ROOT_DIR  --library-dir $LIBRARY_DIR
 
 # then the files are owned
-./pkgr --query owner --library-dir $LIBRARY_DIR usr/bin/hello | \
-    grep -q "^test-1: usr/bin/hello$" || fail "file not owned"
+./pkgr --query owner --library-dir $LIBRARY_DIR /usr/bin/hello | \
+    grep -q "^test-1: /usr/bin/hello$" || fail "file not owned"
