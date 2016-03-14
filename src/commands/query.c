bb402ff1dbfed05e1cdc429efef2c3b0a4a010d4
Hendri Pretorius <pretorh@gmail.com>
2016-03-14 18:40:34 +0200

    query owner fails if some of the files are not found
    return flag indicating if the file was not found in query_owner

--- src/commands/query.c
+++ src/commands/query.c
@@ -3,15 +3,22 @@
 #include "pkgr/library.h"
 #include <stdlib.h>
 
-void query_owner(const char *library, const char *file);
+int query_owner(const char *library, const char *file);
 
 void query(struct Settings *settings) {
     if (strcmp(settings->argument, "installed") == 0) {
         execute("ls %s/", settings->dir_library);
     } else if (strcmp(settings->argument, "owner") == 0) {
+        int not_found = 0;
+
         const char *file;
         while ((file = take_next_argv(settings))) {
-            query_owner(settings->dir_library, file);
+            not_found += query_owner(settings->dir_library, file);
+        }
+
+        if (not_found) {
+            fprintf(stderr, "%d files are not owned\n", not_found);
+            exit(EXIT_FAILURE);
         }
     } else {
         fprintf(stderr, "Unknown query option: %s\n", settings->argument);
@@ -19,9 +26,13 @@ void query(struct Settings *settings) {
     }
 }
 
-void query_owner(const char *library, const char *file) {
+// helpers
+
+int query_owner(const char *library, const char *file) {
     char line[LINE_MAX + 1];
     if (library_get_owner(library, file, line)) {
         printf("%s: %s\n", line, file);
+        return 0;
     }
+    return 1;
 }
