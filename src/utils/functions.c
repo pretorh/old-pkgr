2e7e5b4a82504a63739528571261b1206a6014f9
Hendri Pretorius <pretorh@gmail.com>
2016-03-20 13:45:58 +0200

    if no / in path, use ./ as dir, from as file name

--- src/utils/functions.c
+++ src/utils/functions.c
@@ -2,8 +2,13 @@
 
 int split_filename(const char *from, char *dir, char *file) {
     const char *found = strrchr(from, '/');
-    if (found == NULL)
+    if (found == NULL) {
+        if (dir)
+            strcpy(dir, "./");
+        if (file)
+            strcpy(file, from);
         return 0;
+    }
     int offset = found - from;
 
     if (dir) {
